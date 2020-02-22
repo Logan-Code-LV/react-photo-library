e92ec5c2c3bc06062c5d2672687e8cf2e7483928
Logan Gayler <logangayler@gmail.com>
2020-02-21 16:14:05 -0800

    committwo

--- src/components/AlbumContents.js
+++ src/components/AlbumContents.js
@@ -4,26 +4,29 @@ import axios from "axios"
 
 import PhotoName from "./PhotoName.js"
 
-export default function() {
+export default function(props) {
   const [photos, setPhotos] = useState([])
+  const [albumName, setAlbumName] = useState("")
 
   function fetchPhotos(id) {
-    axios.get(`albums/${id}?_embed=photos`).then(resp => {
-      setPhotos(resp.data)
+    axios.get(`/albums/${id}?_embed=photos`).then(resp => {
+      setPhotos(resp.data.photos)
+      setAlbumName(resp.data.name)
     })
   }
 
   useEffect(() => {
-    fetchPhotos()
+    const id = props.match.params.id
+    fetchPhotos(id)
   }, [])
 
   return (
     <div className="myPhotosContainer">
-      <h1>My Photos</h1>
+      <h1>{albumName}</h1>
       <div className="multiple">
         {photos.map(photo => (
           <Link to={"/PhotoName/" + photo.id}>
-            <img src={photos.url} />
+            <img src={photo.url} />
           </Link>
         ))}
       </div>
